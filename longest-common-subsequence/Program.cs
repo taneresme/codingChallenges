2f196983b05a7e8e4c97647b57c104fc07b31cfa
taneresme <tnr.esme@gmail.com>
2019-07-24 22:15:38 +0300

    Added longest-common-subsequence

---
+++ longest-common-subsequence/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace longest_common_subsequence
+{
+    /*
+Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+Input:
+First line of the input contains no of test cases  T,the T test cases follow.
+Each test case consist of 2 space separated integers A and B denoting the size of string str1 and str2 respectively
+The next two lines contains the 2 string str1 and str2 .
+
+Output:
+For each test case print the length of longest  common subsequence of the two strings .
+
+Constraints:
+1<=T<=200
+1<=size(str1),size(str2)<=100
+
+Example:
+Input:
+2
+6 6
+ABCDGH
+AEDFHR
+3 2
+ABC
+AC
+
+Output:
+3
+2
+
+Explanation
+LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+
+LCS of "ABC" and "AC" is "AC" of length 2
+     */
+    class Program
+    {
+        static int[][] results;
+
+        /* DP solution with recurrence and memoization */
+        static void Main(string[] args)
+        {
+            var tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                Console.ReadLine();
+                var x = Console.ReadLine();
+                var y = Console.ReadLine();
+
+                results = new int[x.Length + 1][];
+                for (int j = 0; j < x.Length + 1; j++)
+                {
+                    results[j] = new int[y.Length + 1];
+                    for (int k = 0; k < y.Length + 1; k++)
+                    {
+                        results[j][k] = -1;
+                    }
+                }
+                
+                Console.WriteLine(lcs(x, y, x.Length, y.Length));
+                Console.ReadKey();
+            }
+        }
+
+        static int lcs(string x, string y, int m, int n)
+        {
+            if (m == 0 || n == 0) return 0;
+            if (results[m][n] > -1) return results[m][n];
+
+            if (x[m - 1].Equals(y[n - 1]))
+            {
+                results[m][n] = lcs(x, y, m - 1, n - 1) + 1;
+                return results[m][n];
+            }
+
+            results[m][n] = Math.Max(lcs(x, y, m - 1, n), lcs(x, y, m, n - 1));
+            return results[m][n];
+        }
+    }
+}
---
+++ longest-common-subsequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("longest-common-subsequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("longest-common-subsequence")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4fb161e-2fd3-453d-9b48-4a2bc2d90da3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
